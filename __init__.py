1c32e1fd25d0e3ea83b3ca149f1e1f73026f88f2
israr <isrargul00@gmail.com>
2021-12-15 20:44:06 +0500

    done

--- __init__.py
+++ __init__.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/contact-us')
+def contact_us():
+    return render_template("contact-us.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
